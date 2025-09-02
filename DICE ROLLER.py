804a3b6aaa7c0cc20ce0a4fe010ce8d9a0f279d9
Narmatha0106 <narmathacse2007@gmail.com>
2025-09-02 22:57:13 +0530

    Add files via upload

---
+++ DICE ROLLER.py
@@ -0,0 +1,14 @@
+import random
+
+def roll_dice():
+    return random.randint(1, 6)
+
+while True:
+    input("Press Enter to roll the dice...")
+    result = roll_dice()
+    print(f"You rolled a {result}!")
+
+    again = input("Roll again? (y/n): ").lower()
+    if again != 'y':
+        print("Thanks for playing!")
+        break
